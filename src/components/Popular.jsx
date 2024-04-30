137874a7c6d195dd96d49ca4b91016f4caf4493b
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2024-04-30 10:16:20 -0230

    styled componenrs

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
     const [popular, setPopular] = useState([]);
@@ -8,25 +9,44 @@ function Popular() {
     }, []);
 
     const getPopular = async () => {
-        const apiKey = process.env.REACT_APP_API_KEY || '9a0c4351054043918751db84eba68ed0';
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=9`);
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        );
+        
         const data = await api.json();
         setPopular(data.recipes);
+        console.log(data.recipes);
     };
-    
 
     return (
-        <div>
-            <>Popular</>
-            {popular.map((recipe) => {
-                return (
-                    <div key={recipe.id}> {/* Added key prop */}
-                        <p>{recipe.title}</p>
-                    </div>
-                );
-            })}
-        </div>
+            <div>
+                {popular.map((recipe) => {
+                    return (
+                        
+                            <Wrapper>
+                                <h3>Popular Picks</h3>
+                                {popular.map((recipe) => {
+                                    return (
+                                        <Card key = {recipe.id} >
+                                            <p>{ recipe.title }</p>
+                                            <img src = { recipe.image } alt = { recipe.title }></img>
+                                        </Card>
+                                    );
+                                })}
+                            </Wrapper>
+                        
+                     );
+                 })}
+            </div>
     );
 }
+
+    const Wrapper = styled.div`
+    margin: 4rem 0rem;
+    ;`
+
+    const Card = styled.div`
+    min-height: 25rem;
+    border-radius: 2rem;`
 
 export default Popular;
