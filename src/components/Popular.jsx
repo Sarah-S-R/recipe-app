ce3c4bd2417ac16909a17dbdfa27f606cd9cd4d4
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2024-04-29 21:39:03 -0230

    api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 
+function Popular() {
+    const [popular, setPopular] = useState([]);
 
-function Popular () {
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        const apiKey = process.env.REACT_APP_API_KEY || '9a0c4351054043918751db84eba68ed0';
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=9`);
+        const data = await api.json();
+        setPopular(data.recipes);
+    };
+    
 
     return (
-        <div>Popular</div>
-    )
+        <div>
+            <>Popular</>
+            {popular.map((recipe) => {
+                return (
+                    <div key={recipe.id}> {/* Added key prop */}
+                        <p>{recipe.title}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
-export default Popular
+export default Popular;
